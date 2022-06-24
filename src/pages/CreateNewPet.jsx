7f5987ac041715fde5425b72c0896ffae59f6869
Katja P <katjapesonen1@gmail.com>
2022-06-24 15:28:03 +0300

    Fix booboo

--- src/pages/CreateNewPet.jsx
+++ src/pages/CreateNewPet.jsx
@@ -1,7 +1,69 @@
-import React from 'react'
-import { useState } from "react";
-import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+    import React from 'react'
+    import { Box, Button, Input, InputWrapper, Title } from '@mantine/core'
+    import { useForm } from '@mantine/form'
+    import { useNavigate } from 'react-router-dom'
+    import { creatingPet } from '../utils/helper'
+    
+    
+    function CreateNewPet() {
+      const navigate = useNavigate()
+      const form = useForm({
+        initialValues: {
+          name: '',
+          type: '',
+          age: '',
+          image: '',
+          description: '',
+        },
+      })
+    
+      const createPet = async newPet => {
+        try {
+          const response = await creatingPet(newPet) // 
+          console.log(response, 'createPet')
+    
+          if (response.status === 'KO') {
+            throw new Error(response.message)
+          }
+    
+          navigate('/user/profile')
+        } catch (error) {
+          form.setErrors({ username: error.message })
+        }
+      }
+    
+    
+      const handleSubmit = values => {
+        createPet(values)
+      }
+    
+      return (
+        <div>
+            <Box>
+          <Title>Create New Pet</Title>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <InputWrapper
+              required
+              label='Name'
+              description='Your pets name'
+            >
+              <Input {...form.getInputProps('name')} />
+            </InputWrapper>
+    
+    
+            <InputWrapper
+              required 
+              label='Type'
+              description='Type of animal'
+            >
+              <Input {...form.getInputProps('type')} />
+            </InputWrapper>
+    
+    
+            <InputWrapper 
+               label='Age' description='Your pets age'>
+              <Input {...form.getInputProps('age')} />
+            </InputWrapper>
 
 function CreateNewPet(props) {
 
