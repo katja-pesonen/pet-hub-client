54e0c31ee6bb98bf24384a58cd22bcca677515f3
Katja P <katjapesonen1@gmail.com>
2022-06-30 16:00:43 +0300

    Home page styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ function HomePage() {
     <div className='heroSection'>
 
       <div className='welcome-header' style={{backgroundColor: '#3fada8', height:'300'}}>
-         <h1 style={{fontSize:'50'}}>Hello there, welcome to Pet Hub!</h1>
+         <h1>Hello there, welcome to Pet Hub!</h1>
          <p>Pet Hub is a fun website where you can view profiles of peoples pets, and upload your own pets too! Sign up & log in to add your furry friends to the site!</p>
       </div>
 
